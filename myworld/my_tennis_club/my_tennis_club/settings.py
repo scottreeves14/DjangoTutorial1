d3dd8fbda6da14d1e51008e1b71f4fae08a3beff
Scott Reeves <scott.reeves14@det.nsw.edu.au>
2025-10-22 01:07:45 +0000

    Checked in after testing

--- myworld/my_tennis_club/my_tennis_club/settings.py
+++ myworld/my_tennis_club/my_tennis_club/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-u4i=o+h%346637h@!rf1_o^2#gal8qr83kkcrt0fo-jsqz%+(*"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
